b13819c621b2122a9af95e8b6b3951ee45949221
Gabriel <gcasray@iesmartinezm.com>
2023-02-11 19:19:33 +0100

    Corrección de errores, implementación de las dependencias en gradle.build y clase conexion

--- SocialDriveMM/app/src/main/java/iesmm/pmdm/socialdrivemm/MapsActivity.java
+++ SocialDriveMM/app/src/main/java/iesmm/pmdm/socialdrivemm/MapsActivity.java
@@ -176,7 +176,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         alertDialog.setNegativeButton("Cancelar",
                 new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
+                public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
                     }
                 });
